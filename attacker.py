FIX loss calculation on batch, random shuffling at the end of epoch

attacker.py
@@ -27,8 +27,8 @@ MODEL = 'efficientdet-lite4'
 class PatchAttacker(tf.keras.Model):
     """attack with malicious patches"""
 
-    def __init__(self, model: efficientdet_keras.EfficientDetModel, patch_loss_multiplier=0., iou=.5,
-                 min_patch_height=60, visualize_freq=200):
+    def __init__(self, model: efficientdet_keras.EfficientDetModel, patch_loss_multiplier=1e-2, iou=.5,
+                 min_patch_height=1, visualize_freq=200):
         super().__init__(name='Attacker_Graph')
         self.model = model
         self.config = self.model.config
@@ -118,7 +118,7 @@ class PatchAttacker(tf.keras.Model):
 
         with tf.GradientTape(persistent=True) as tape:
             boxes_pred, scores, classes = self.second_pass(self._images)
-            loss = tf.reduce_max(scores)
+            loss = tf.reduce_sum(tf.reduce_max(scores, axis=1))
             tv_loss = self.tv_loss()
 
         self.add_loss(loss)
@@ -281,8 +281,8 @@ class Patcher(tf.keras.layers.Layer):
         h = ymax - ymin
         w = xmax - xmin
 
-        patch_h = h * self.scale
-        patch_w = self.aspect * patch_h
+        patch_w = h * self.scale
+        patch_h = self.aspect * patch_w
 
         orig_y = ymin + h / 2.
         orig_x = xmin + w / 2.

train_data_generator.py
@@ -84,10 +84,16 @@ class COCOPersonsSequence(tf.keras.utils.Sequence):
         if self._shuffle:
             np.random.shuffle(self._flist)
 
-        for i in range(len(self)):
+        i = 0
+        while True:
             image, boxes = self[i]
             yield (tf.convert_to_tensor(image, dtype=tf.float32),
                    tf.RaggedTensor.from_tensor(tf.convert_to_tensor(boxes, dtype=tf.float32)))
+            i += 1
+            if i == len(self):
+                if self._shuffle:
+                    np.random.shuffle(self._flist)
+                i = 0
 
 
 def _parse_line(line):
@@ -102,6 +108,7 @@ def _read_image(img_dir, filename):
 
 
 def filter_by_dims(img_dir, label_dir, min_height_ratio, min_width_ratio, aspect, filename):
+    return True
     im = _read_image(img_dir, filename)
     h, w, _ = im.shape
     filename = os.path.extsep.join([os.path.splitext(filename)[0], 'txt'])
@@ -139,7 +146,7 @@ def partition(config, img_dir, label_dir, min_height_ratio=.7, min_width_ratio=.
                                    file_list=file_list[start:end], shuffle=shuffle)
         return tf.data.Dataset.from_generator(dseq, output_signature=(
             tf.TensorSpec(shape=(*output_size, 3), dtype=tf.float32),
-            tf.RaggedTensorSpec(shape=(None, 4), dtype=tf.float32))).batch(batch_size).repeat(-1).prefetch(10)
+            tf.RaggedTensorSpec(shape=(None, 4), dtype=tf.float32))).batch(batch_size).prefetch(10)
 
     train_ds = get_tf_dataset(0, train_size)
     val_ds = get_tf_dataset(train_size, train_size+val_size)